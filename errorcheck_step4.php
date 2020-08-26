aaa3ddaf72874955ba05d2d547ac9c63056d1bfc
lightsalt9610 <69782886+lightsalt9610@users.noreply.github.com>
2020-08-26 15:26:24 +0900

    Add files via upload

---
+++ errorcheck_step4.php
@@ -0,0 +1,43 @@
+<?php
+    include "setupsquare_step1.php";
+
+    $width = 10;
+    $height = 10;
+
+    setup_square($width, $height);
+
+    $gameover = TRUE;
+    $already[] = 500;
+
+    while ($gameover) {
+    echo "\nselect square:";
+    $trim = trim(fgets(STDIN));
+    $choose = explode(",", $trim);
+    
+    if (in_array(FALSE,$choose) || $width < $choose[0] || $height < $choose[1]) {
+        print "you enter inregal number.";
+        continue;
+    }
+
+    for ($i = 0; $i < $width * $height; $i++) {
+        if ($i % $width == 0) {
+            echo "\n";
+        }
+
+        if ($i == ((($choose[0] * 10) - 10) + $choose[1]) - 1) {
+            echo $number[$i] . " ";
+            $appeared = $number[$i];
+            $already[] = $i;
+        } else if (in_array($i, $already)){
+            echo $number[$i] . " ";
+        } else {
+            echo "■";
+        }
+    }
+        if ($appeared == 5) {
+            $gameover = FALSE;
+        }
+    }
+
+    echo"\n\nGAME OVER\n";
+?>
