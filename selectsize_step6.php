7991d791cff10ccb437d5b896f5961a37f5ce772
lightsalt9610 <69782886+lightsalt9610@users.noreply.github.com>
2020-08-26 15:33:36 +0900

    Update selectsize_step6.php

--- selectsize_step6.php
+++ selectsize_step6.php
@@ -1,18 +1,20 @@
 <?php
+#ステップ４のプログラムがエラーまみれだったので、とりあえずゲームとして成立するように仕上げておく
     include "setupsquarefrexi_step5.php";
-
+    #サイズを標準入力で取るようにした
     print"set square width size:";
     $width = trim(fgets(STDIN));
     print"set square height size:";
     $height = trim(fgets(STDIN));
+    #ハズレも指定 
     print"set avoidnumber:";
     $avoidnumber = trim(fgets(STDIN));
 
     setup_square($width, $height);
     numbergame_avoid($width,$height,$avoidnumber);
-
+    #関数化
     function numbergame_avoid($width,$height,$avoidnumber){
-        
+        #もしもハズレがありえない数字にしていたら強制終了するように
         if($avoidnumber > 9 || $avoidnumber < 1) {
             exit("\nplease enter number in 1~9");
         }
@@ -24,7 +26,7 @@
         echo "\nselect square:";
         $trim = trim(fgets(STDIN));
         $choose = explode(",", $trim);
-        
+        #サイズが可変になったのでこの辺りも可変するように
         if (in_array(FALSE,$choose) || $width < $choose[0] || $height < $choose[1]) {
             print "you enter inregal number.";
             continue;
@@ -48,7 +50,7 @@
             if ($appeared == $avoidnumber) {
                 $gameover = FALSE;
             }
-
+            #全部開けられたらクリア
             if (count($already) > $width * $height) {
                 exit("\n\nGAME CLEAR!\n");
             }
