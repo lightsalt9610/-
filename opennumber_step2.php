3dc155a638badc56eb1adeaea552db76263365bc
lightsalt9610 <69782886+lightsalt9610@users.noreply.github.com>
2020-08-26 15:15:51 +0900

    Update opennumber_step2.php

--- opennumber_step2.php
+++ opennumber_step2.php
@@ -1,11 +1,14 @@
 <?php
+#ステップ１で作ったプログラムを使い、指定した場所のマスを開けるプログラム。
+
     include "setupsquare_step1.php";
 
+    #サイズを指定
     $width = 10;
     $height = 10;
-
+    
     setup_square($width, $height);
-
+    #開ける場所はカンマ区切りで数字二つを指定する型にした。
     echo "\nselect square:";
     $trim = trim(fgets(STDIN));
     $choose = explode(",", $trim);
@@ -14,7 +17,7 @@
         if ($i % $width == 0) {
             echo "\n";
         }
-
+        #指定した場所なら、そのマスに格納された数字を表示する
         if ($i == ((($choose[0] * 10) - 10) + $choose[1])) {
             echo $number[$i] . " ";
         } else {
